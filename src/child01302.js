46b99dd27c1eb7532c3cc1a3d72f58d0b982aebc
ofroad <wh724724@qq.com>
2018-07-04 11:24:55 +0800

    使用forwardRef

--- src/child01302.js
+++ src/child01302.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-export default class child013 extends Component {
+export default class child01302 extends Component {
   constructor(props) {
     super(props);
-	this.p = React.createRef();
   }
   render() {
+	
+	const MyButton = React.forwardRef((props, ref) => (
+	  <p ref={ref}>
+		{props.children}
+		i am child01302
+	  </p>
+	));
+	console.log(MyButton);
     return (
       <div className="box-links">
-		<p ref={this.p}>i am child01302</p>
+		<MyButton />
       </div>
     );
   }
--- src/p13.js
+++ src/p13.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Childlinks from "./childlink";
 import Child01301 from "./child01301";
+//import Child01302 from "./child01302";
 import './App.css';
 
 console.log(React);
@@ -11,6 +12,7 @@ class p13 extends React.Component {
     this.state = {showWarning: true};
 	this.textInput = React.createRef();
 	this.child = React.createRef();
+	this.child02 = React.createRef();
   }
 
   componentWillMount(){
@@ -35,11 +37,21 @@ class p13 extends React.Component {
 	  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
 	  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
 	];
+	
+	//似乎不好在class组件 Child01302中定义
+	const MyButton = React.forwardRef((props, ref) => (
+	  <p ref={ref}>
+		{props.children}
+		i am child01302
+	  </p>
+	));
+	
     return (
       <div>
 		<h1>ref---createRef</h1>
         <div>name: <input type="text" ref={this.textInput} /></div>
 		<Child01301 ref={this.child} />
+		<MyButton ref={this.child02} />
 		<Childlinks />
       </div>
     );
@@ -51,8 +63,11 @@ class p13 extends React.Component {
 	  //通过current访问dom
 	  this.textInput.current.focus();
 	  
-	  //拿到子组件Child013里面的p节点
+	  //拿到子组件Child0131里面的p节点
 	  console.log(this.child.current.p.current.innerHTML);
+	  
+	  //拿到子组件Child0132里面的p节点
+	  console.log(this.child02.current.innerHTML);
   }
   componentWillUnmount(){
 	  var that=this;
